e3ab51c05dbf3eb8f34dc70071ef67f13d82d903
Katherine Aguilera <katherine.aguilera@huelladigital.tv>
2020-02-10 14:11:09 -0500

    Inputs y Outputs

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,22 +1,19 @@
-// Decoradores
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Product } from '../product.model';
 
-
-// Component
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html'
 })
 export class ProductComponent {
 
-  product: Product = {
-    id: '1',
-    image: 'assets/images/camiseta.png',
-    title: 'Camiseta',
-    price: 80000,
-    description: 'bla bla bla bla bla'
-  };
+  @Input() product: Product;
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  addCart() {
+    console.log('añadir al carrito');
+    this.productClicked.emit(this.product.id);
+  }
 
 }
