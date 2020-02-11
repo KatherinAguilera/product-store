cf984268fa871c4708e3705daf5204acb753e95f
Katherine Aguilera <katherine.aguilera@huelladigital.tv>
2020-02-11 11:32:57 -0500

    Rutas redirect/pagenotfound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,11 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
-import { CartComponent } from './cart/cart.component' 
+import { CartComponent } from './cart/cart.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component' 
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +27,11 @@ import { CartComponent } from './cart/cart.component'
     ProductsComponent,
     ContactComponent,
     DemoComponent,
-    CartComponent
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
